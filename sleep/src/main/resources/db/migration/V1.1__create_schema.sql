8d329af49ff4157efb05120a6094df4ac76f1d06
lukaslopes <lukas.antunes.lopes@gmail.com>
2024-09-03 17:06:16 -0300

    Creating database for sleeplog on flyway script

---
+++ sleep/src/main/resources/db/migration/V1.1__create_schema.sql
@@ -0,0 +1,10 @@
+CREATE TABLE daily_sleep_log (
+    id INT PRIMARY KEY,
+    sleep_start TIMESTAMP NOT NULL,
+    sleep_end TIMESTAMP NOT NULL,
+    sleep_quality INT NOT NULL,
+    sleep_duration INT NOT NULL,
+    user_id INT NOT NULL, -- for this test, user is not a foreign key
+    created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
+    modified_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP
+);
